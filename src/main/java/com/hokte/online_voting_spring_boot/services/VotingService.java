a22bb4100363111d29d71b69210e553d48d2b46c
Yash Hokte <hokteyash4@gmail.com>
2026-01-13 23:15:37 +0530

    Added VotingService class

---
+++ src/main/java/com/hokte/online_voting_spring_boot/services/VotingService.java
@@ -0,0 +1,19 @@
+package com.hokte.online_voting_spring_boot.services;
+
+import com.hokte.online_voting_spring_boot.repo.CandidateRepo;
+import com.hokte.online_voting_spring_boot.repo.VoteRepo;
+import com.hokte.online_voting_spring_boot.repo.VoterRepo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class VotingService {
+    private VoteRepo voteRepo;
+    private CandidateRepo candidateRepo;
+    private VoterRepo voterRepo;
+
+    public VotingService(VoteRepo voteRepo, CandidateRepo candidateRepo, VoterRepo voterRepo) {
+        this.voteRepo = voteRepo;
+        this.candidateRepo = candidateRepo;
+        this.voterRepo = voterRepo;
+    }
+}
