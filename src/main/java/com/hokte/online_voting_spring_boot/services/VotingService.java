2a6d206a3e38cbee212918262485697ce8c7b0e6
Yash Hokte <hokteyash4@gmail.com>
2026-01-18 23:11:20 +0530

    Added caste vote logic

--- src/main/java/com/hokte/online_voting_spring_boot/services/VotingService.java
+++ src/main/java/com/hokte/online_voting_spring_boot/services/VotingService.java
@@ -1,7 +1,11 @@
 package com.hokte.online_voting_spring_boot.services;
 
 import com.hokte.online_voting_spring_boot.dto.VoteRequestDto;
+import com.hokte.online_voting_spring_boot.exception.ResourceNotFoundException;
+import com.hokte.online_voting_spring_boot.exception.VoteNotAllowedException;
+import com.hokte.online_voting_spring_boot.models.Candidate;
 import com.hokte.online_voting_spring_boot.models.Vote;
+import com.hokte.online_voting_spring_boot.models.Voter;
 import com.hokte.online_voting_spring_boot.repo.CandidateRepo;
 import com.hokte.online_voting_spring_boot.repo.VoteRepo;
 import com.hokte.online_voting_spring_boot.repo.VoterRepo;
@@ -19,7 +23,26 @@ public class VotingService {
         this.voterRepo = voterRepo;
     }
 
-    public Vote castVote(VoteRequestDto voteRequestDto) {
-        return new Vote();
+    public Vote castVote(Long voterId, Long candidateId) {
+        if(!voterRepo.existsById(voterId)){
+            throw new ResourceNotFoundException("Voter with id " + voterId + " not found");
+        }
+        if(!candidateRepo.existsById(candidateId)){
+            throw new ResourceNotFoundException("Candidate with id " + candidateId + " not found");
+        }
+        Voter voter = voterRepo.findById(voterId).get();
+        if(voter.isHasVoted()){
+            throw new VoteNotAllowedException("Voter with id " + voterId + " is already voted");
+        }
+        Candidate candidate = candidateRepo.findById(candidateId).get();
+        Vote vote = new Vote();
+        vote.setCandidate(candidate);
+        vote.setVoter(voter);
+        voteRepo.save(vote);
+        candidate.setVoteCount(candidate.getVoteCount() + 1);
+        candidateRepo.save(candidate);
+        voter.setHasVoted(true);
+        voterRepo.save(voter);
+        return vote;
     }
 }
